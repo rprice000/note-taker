67390b25aa388f157ce0039acd3338e60ad8a6ed
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-07 17:06:59 -0600

    fixed notes display with splice(1)

--- server.js
+++ server.js
@@ -16,17 +16,43 @@ app.use(express.json());
 // links css/javascript files for HTML pages
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-// Extracts notes from json file to be displayed on notes.html
-    res.json(notesRoutes.slice(0));
-});
 
+function generateNote(body, noteTakerArray ) {
+    const note = body;
+    if (!Array.isArray(noteTakerArray))
+        noteTakerArray = [];   
+
+    if(noteTakerArray.length === 0)
+        // Starts array at object '1'
+        noteTakerArray.push(1);
+        // Provides unique id to each array object
+        body.id = noteTakerArray[0];
+        // Increases unqiue id by 1 each time array object is created
+        noteTakerArray[0]++;
+        // Pushes new note object to note taker array
+        noteTakerArray.push(note);
+        // takes notes and writes to json file
+        fs.writeFileSync(path.join(__dirname, './db/db.json'),
+        // creates User Notes database for notes to be stored
+            JSON.stringify({ userNotes: noteTakerArray }, null, 2)
+    );
+
+
+    return note;
+}
+
+
+
+
+app.get('/api/notes', (req, res) => {
+// Extracts notes from json file to be displayed on notes.html "1" starts the extract at 2nd Object in the array
+    res.json(notesRoutes.slice(1));
+});
 
 // Route to get index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
 
 // Route to get notes.html
 app.get('/notes', (req, res) => {
@@ -38,29 +64,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-function generateNote(body, noteTakerArray ) {
-    const note = body;
-    if(noteTakerArray.length === 0)
-        // Starts array at object '1'
-        noteTakerArray.push(1);
-        body.id = noteTakerArray[0];
-        noteTakerArray[0]++;
-        noteTakerArray.push(note);
-        // takes notes and writes to json file
-        fs.writeFileSync(path.join(__dirname, './db/db.json'),
-        // creates User Notes database for notes to be stored
-            JSON.stringify({ userNotes: noteTakerArray }, null, 2)
-    );
-
-
-    return body;
-}
-
 app.post('/api/notes', (req, res) => {
     const note = generateNote(req.body, notesRoutes);
     res.json(note);
 });
-
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
