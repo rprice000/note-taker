0f4e353413376171a7ab081233bc9441108c2e0b
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-07 13:30:46 -0600

    changed app.listen

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express');
+
 const app = express();
 
+const notesRoutes = require('.db/db.json');
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+// parse incoming strng or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+// links css/javascript files for HTML pages
+app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
+// app.listen(3001, () => {
+//     console.log(`API server now on port 3001!`);
+//   });
